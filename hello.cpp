6c6470413531bef75a2573ae785d226b068ed441
ZakLake <zak.lake0@gmail.com>
2018-11-29 14:55:16 -0800

    update

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+// hello.cpp 
+#include "cmpt_error.h"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   cout << "Hello, CMPT 130!\n";
+} // main
+
+
+// Ubuntu 
+// cd ../../mnt/c/Andy/Astellarius/optimusCpp
+
+// Use make to compile your program:
+// $ make hello
+// Assuming there are no errors, run the resulting program like this:
+// $ ./hello
